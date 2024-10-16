b36618438935bb86563c34e35070dbfb7ec30fa6
BIGG <elijahokezie4@gmail.com>
2024-10-16 11:18:32 -0700

    rockpaperscissors.py

--- flowcontril.py
+++ flowcontril.py
@@ -1,4 +1,4 @@
-# This appallows you to create a username, password and asks you to login with it
+# This app allows you to create a username, password and asks you to login with it
 
 # create username and password
 print("hello user, create your name")
@@ -7,11 +7,7 @@ print("Awesome! now create a password")
 password = input()
 
 # Login with the username and password created
-print("...................................................................")
-print("you have successfully signed up. please login with your credentials")
-print("...................................................................")
-userlogin = input("enter username here >>:")
-# if userlogin == username:
+
 #     print("nice one!")
 #     userpassword = input("enter password: ")
 #     if userpassword == password:
@@ -20,12 +16,3 @@ userlogin = input("enter username here >>:")
 #         print("wrong password. try again")
 # else:
 #     print("wrong user name.. try again")
-
-while userlogin != username:
-    print("wrong username, please try again")
-    userlogin=input()
-userpassword = input("please enter your password: ")
-while userpassword != password:
-    print("wrong password plaese try again: ")
-    userpassword = input()
-print(f"nice work,{username}. Access granted!")
--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -4,26 +4,56 @@
 import random
 choice_list = ['rock','paper','scissors']
 app_choice = random.choice(choice_list) #app chooses from the list...
-print(app_choice)
 
 # initiate users choice
-user_choice = input("welcome to the game. Rock, Paper or Scissors?: ").capitalize()
+# user_choice = input("welcome to the game. Rock, Paper or Scissors?: ").capitalize()
 
 
+# # entry validation
+# while user_choice not in ["Rock","Paper","Scissors"]:
+#     user_choice = input("invalid entry. Enter Rock, Paper or Scissors: ").capitalize()
 
-# entry validation
-while user_choice not in ["Rock","Paper","Scissors"]:
-    user_choice = input("invalid entry. Enter Rock, Paper or Scissors: ").capitalize()
+# # compare user entry and user choice and determine winner
+
+# if user_choice == app_choice:
+#     print(f"its a draw! we both chose {app_choice}!")
+# elif(
+#     user_choice == "Rock" and app_choice == "scissors" or
+#     user_choice == "scissors" and app_choice == "Paper" or
+#     user_choice == "Paper" and app_choice == "Rock"
+# ):
+#     print(f"you won! you chose {user_choice} and i chose {app_choice}.")
+# else:
+#     print(f"i won! you chose {user_choice} and i chose {app_choice}.")
 
 # compare user entry and user choice and determine winner
 
-if user_choice == app_choice:
-    print(f"its a draw! we both chose {app_choice}!")
-elif(
-    user_choice == "Rock" and app_choice == "scissors" or
-    user_choice == "scissors" and app_choice == "Paper" or
-    user_choice == "Paper" and app_choice == "Rock"
-):
-    print(f"you won! you chose {user_choice} and i chose {app_choice}.")
-else:
-    print(f"i won! you chose {user_choice} and i chose {app_choice}.")
+while True:
+    rounds = int(input("How many time would you like to play? Enter a number: "))
+    if (rounds <2
+        or rounds > 10):
+        print("enter a number between 2-10. Try again")
+        continue
+    else:
+        print("welcome to the game. Lets play!")
+
+    for round in range(rounds):
+        user_choice = input("Rock, Paper or scissors: ").capitalize()
+
+        # Match the entries and determine a winner
+        if user_choice == app_choice:
+            print(f"it is a draw! We both chose {app_choice}")
+        elif(
+            user_choice == "Rock" and app_choice == "scissors" or
+            user_choice == "paper" and app_choice == "Rock" or
+            user_choice == "scissors" and app_choice == "paper"
+        ):
+            print(f"you won. you chose {user_choice} and i chose {app_choice}.")
+        else:
+            print(f"i won, i chose {app_choice} and you chose {user_choice}.")
+    play_again = input(f"you have played {rounds} games. would you like to play again? (yes or no): ")
+    if play_again == 'yes'.lower():
+        continue
+    else:
+        print("Thanks for playing. Bye.")
+        break
