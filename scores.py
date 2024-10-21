033711373aa344959ffbc76276778e2c4ea1ba8a
BIGG <elijahokezie4@gmail.com>
2024-10-21 12:07:46 -0700

    scores.py

---
+++ scores.py
@@ -0,0 +1,8 @@
+# create a list of scores
+scores =[45,67,34,23,11,90,87,45,45,45,100,99,87,22]
+secondscore =scores[2]
+print(secondscore)
+
+
+for i in range(len(scores)):
+    print(f"student No: {i} scored:{scores[i]}")
